a506a4b5e7ce395e85f4f3c573a3abf220c23145
MirkoGLostia <giggi.torre@mail.com>
2023-06-06 11:34:11 +0200

    header base

---
+++ src/components/HeaderComics.vue
@@ -0,0 +1,34 @@
+<script>
+import HeaderMenu from './HeaderMenu.vue';
+
+export default {
+  name: "HeaderComics",
+  components: {
+    HeaderMenu
+  }
+}
+</script>
+
+<template>
+  <header class="container flexStyle">
+    <div>
+      <img src="./../assets/img/dc-logo.png" alt="logo">
+    </div>
+    <div>
+      <HeaderMenu />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.container {
+  width: 80%;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.flexStyle {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,82 @@
+<script>
+export default {
+  name: "HeaderMenu"
+}
+</script>
+
+<template>
+  <section class="flexStyle menuContainer">
+    <div>
+      <span>
+        characters
+      </span>
+    </div>
+
+    <div>
+      <span>
+        comics
+      </span>
+    </div>
+
+    <div>
+      <span>
+        movies
+      </span>
+    </div>
+
+    <div>
+      <span>
+        tv
+      </span>
+    </div>
+
+    <div>
+      <span>
+        games
+      </span>
+    </div>
+
+    <div>
+      <span>
+        collectibles
+      </span>
+    </div>
+
+    <div>
+      <span>
+        videos
+      </span>
+    </div>
+
+    <div>
+      <span>
+        fans
+      </span>
+    </div>
+
+    <div>
+      <span>
+        news
+      </span>
+    </div>
+
+    <div>
+      <span>
+        shop
+      </span>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.flexStyle {
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.menuContainer div {
+  padding: 20px;
+}
+</style>
