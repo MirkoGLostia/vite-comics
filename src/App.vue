93a959117e4ae538ebffbaa2f818910d54e699d7
MirkoGLostia <giggi.torre@mail.com>
2023-06-06 21:49:29 +0200

    origin vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <script>
 import HeaderComics from './components/HeaderComics.vue'
+import MainComics from './components/MainComics.vue';
+import FooterComics from './components/FooterComics.vue';
 
 export default {
   components: {
     HeaderComics,
+    MainComics,
+    FooterComics
   }
 }
 
 </script>
 
 <template>
-  <HeaderComics />
+  <header>
+    <HeaderComics />
+    <MainComics />
+    <FooterComics />
+  </header>
 </template>
 
 <style lang="scss">
-@use './styles/general.scss'
+@use './styles/partials/variables' as *;
+@use './styles/general.scss' as *;
 </style>
