b04df5ae3022ab7b037af2b7f61f5d029c263d42
MirkoGLostia <giggi.torre@mail.com>
2023-06-06 21:49:47 +0200

    header

--- src/components/HeaderComics.vue
+++ src/components/HeaderComics.vue
@@ -1,34 +1,126 @@
 <script>
-import HeaderMenu from './HeaderMenu.vue';
-
 export default {
   name: "HeaderComics",
-  components: {
-    HeaderMenu
+  data() {
+    return {
+      links: [
+        {
+          id: 1,
+          text: 'CHARACTERS',
+          url: '#',
+          current: false
+        },
+        {
+          id: 2,
+          text: 'COMICS',
+          url: '#',
+          current: true
+        },
+        {
+          id: 3,
+          text: 'MOVIES',
+          url: '#',
+          current: false
+        },
+        {
+          id: 4,
+          text: 'TV',
+          url: '#',
+          current: false
+        },
+        {
+          id: 5,
+          text: 'GAMES',
+          url: '#',
+          current: false
+        },
+        {
+          id: 6,
+          text: 'COLLECTIBLES',
+          url: '#',
+          current: false
+        },
+        {
+          id: 7,
+          text: 'VIDEOS',
+          url: '#',
+          current: false
+        },
+        {
+          id: 8,
+          text: 'FANS',
+          url: '#',
+          current: false
+        },
+        {
+          id: 9,
+          text: 'NEWS',
+          url: '#',
+          current: false
+        },
+        {
+          id: 10,
+          text: 'SHOP',
+          url: '#',
+          current: false
+        }
+      ]
+    }
   }
 }
 </script>
 
 <template>
-  <header class="container flexStyle">
-    <div>
-      <img src="./../assets/img/dc-logo.png" alt="logo">
-    </div>
-    <div>
-      <HeaderMenu />
+  <header>
+    <div class="container">
+      <div class="headLogo">
+        <img src="./../assets/img/dc-logo.png" alt="logo">
+      </div>
+
+      <nav>
+        <ul>
+          <li v-for="link in links" :key="link.id">
+            <a :class="link.current ? 'active' : ''" :href="link.url">{{ link.text }}</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </header>
 </template>
 
-<style scoped>
-.container {
-  width: 80%;
-  margin: 0 auto;
-  padding: 20px;
-}
+<style lang="scss" scoped>
+@use './../styles/partials/variables' as v;
+@use './../styles/partials/mixins.scss' as *;
 
-.flexStyle {
-  display: flex;
-  justify-content: space-between;
+header {
+  height: 100px;
+  border-bottom: 1px solid black;
+
+  .container {
+    @include myFlex;
+    @include myFlexPosition;
+
+    .headLogo {
+      width: 70px;
+    }
+
+    ul {
+      display: flex;
+
+      li {
+        font-size: 16px;
+
+        a {
+          color: v.$myBlack;
+          padding: 35px 12px;
+
+          &.active,
+          &:hover {
+            color: v.$myBlue;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
